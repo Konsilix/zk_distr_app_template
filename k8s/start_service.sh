45fb0cc06418673da08d4fbd8fc1a7bbc6ec9cdd
Mohamed0510 <144189711+Mohamed0510@users.noreply.github.com>
2025-08-02 15:13:34 -0400

    Update start_service.sh

--- k8s/start_service.sh
+++ k8s/start_service.sh
@@ -2,11 +2,10 @@
 set -e
 
 echo "Starting the application service..."
-# The following line was removed as it is not needed when using Helm.
-# docker compose up -d
 
-# Helm deployment command would go here, for example:
-# helm upgrade --install chatbot ./helm/chatbot
+# The 'docker compose up -d' command has been removed.
+# The following line deploys your application to Kubernetes using Helm.
+helm upgrade --install chatbot ./helm/chatbot
 
 # Check the exit status of the last command
 if [ $? -eq 0 ]; then
