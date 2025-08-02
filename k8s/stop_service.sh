9c044f2a11b52261c7f1de7d6e1220aa1dfb8c1e
Mohamed0510 <144189711+Mohamed0510@users.noreply.github.com>
2025-08-02 12:47:45 -0400

    Update stop_service.sh

--- k8s/stop_service.sh
+++ k8s/stop_service.sh
@@ -5,7 +5,7 @@ echo "Stopping the application service..."
 # The following line was removed as it is not needed when using Helm.
 # docker compose down
 
-# Your Helm undeployment command would go here, for example:
+# Helm undeployment command would go here, for example:
 # helm uninstall chatbot
 
 # Check the exit status of the last command
