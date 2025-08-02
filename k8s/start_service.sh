83fa632abdfb6c6b77fddf520878e64ae8b024be
Mohamed0510 <144189711+Mohamed0510@users.noreply.github.com>
2025-08-02 12:47:32 -0400

    Update start_service.sh

--- k8s/start_service.sh
+++ k8s/start_service.sh
@@ -2,11 +2,15 @@
 set -e
 
 echo "Starting the application service..."
-docker compose up -d
+# The following line was removed as it is not needed when using Helm.
+# docker compose up -d
+
+# Helm deployment command would go here, for example:
+# helm upgrade --install chatbot ./helm/chatbot
 
 # Check the exit status of the last command
 if [ $? -eq 0 ]; then
     echo "Application service started successfully."
 else
-    echo "Application service via Docker failed!"
+    echo "Application service via Helm failed!"
 fi
